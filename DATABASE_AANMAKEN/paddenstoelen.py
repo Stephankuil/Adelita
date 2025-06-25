8cfb8c005a5a9413647c0a3ae74f7defec28b50b
stephan_kuil_0894873_Happy_Pancaker <0894873@hr.nl>
2025-06-25 17:44:55 +0200

    paddenstoelen toevoegen met python lijst

---
+++ DATABASE_AANMAKEN/paddenstoelen.py
@@ -0,0 +1,775 @@
+paddenstoelen_info = [
+  {
+    "naam_nl": "Amandelpaddenstoel",
+    "latijns": "Agaricus blazei murill",
+    "familie": "Agaricaceae",
+    "namen": {
+      "Japans": "Himematsutake",
+      "Chinees/TCM": "Jisongrong"
+    },
+    "stoffen": [
+      "Ergocalciferol",
+      "bèta-glucanen",
+      "cytochroom P-450",
+      "katalase",
+      "xylanase"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "pancreas",
+      "milt",
+      "lever",
+      "bloed",
+      "hersenen",
+      "zintuigen",
+      "huid",
+      "hart en vaatstelsel",
+      "maag-darmstelsel",
+      "geslachtsorganen",
+      "skelet"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antiviraal en antibacterieel",
+      "antioxidant",
+      "ontstekingsremmend",
+      "cholesterolverlagend",
+      "slijmoplossend",
+      "aromataseremmer",
+      "rustgevend",
+      "anti-aging",
+      "anti-inflammatoir",
+      "histamine verlagend",
+      "bloeddrukverlagend",
+      "regulering bloedsuikerspiegel",
+      "stimulatie neurogenese"
+    ]
+  },
+  {
+    "naam_nl": "Antrodia",
+    "latijns": "Antrodia cinnamomea",
+    "familie": "Fomitopsidaceae",
+    "namen": {
+      "Taiwan": "Niu Chang Ku"
+    },
+    "stoffen": [
+      "Polysacchariden",
+      "antrodioxolanone",
+      "antcin B",
+      "antrocampin B",
+      "antioxidanten",
+      "triterpenen",
+      "adenosine",
+      "superoxide dismutase",
+      "immunoglobuline",
+      "steroïden"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "lever",
+      "longen",
+      "huid",
+      "prostaat",
+      "nieren",
+      "vrouwelijke geslachtsorganen"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antioxidant",
+      "regulering metabolisme",
+      "antiviraal",
+      "ontstekingsremmend"
+    ]
+  },
+  {
+    "latijnse_naam": "Antrodia cinnamomea",
+    "synoniemen": [
+      "Antrodia camphorata",
+      "Taiwanofungus camphoratum",
+      "Ganoderma camphoratum"
+    ]
+  },
+
+
+  {
+    "familie": "Fomitopsidaceae",
+    "nederlandse_naam": "Antrodia",
+    "lokale_naam_taiwan": "Niu Chang Ku",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Antrodioxolanone",
+      "Antcin B",
+      "Antrocampin B",
+      "Antioxidanten",
+      "Triterpenen",
+      "Adenosine",
+      "Superoxide dismutase",
+      "Immunoglobuline",
+      "Steroïden"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "lever",
+      "longen",
+      "huid",
+      "prostaat",
+      "nieren",
+      "vrouwelijke geslachtsorganen"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antioxidant",
+      "regulering metabolisme",
+      "antiviraal",
+      "ontstekingsremmend"
+    ]
+  },
+  {
+    "latijnse_naam": "Armillaria mellea",
+    "familie": "Physalacriaceae",
+    "nederlandse_naam": "Honingzwam",
+    "japanse_naam": "Narataka",
+    "chinese_naam_tcm": "Mi Huan Jun",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysaccharide armillirikine",
+      "Armillaridine",
+      "Tryptamine",
+      "L-tryptofaan",
+      "Serotonine",
+      "Nucleoside derivaten"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "neurologische aandoeningen",
+      "aandoeningen zenuwstelsel",
+      "slapeloosheid",
+      "epilepsie",
+      "hoge bloeddruk",
+      "neurasthenie (zenuwzwakte)",
+      "neuropathie",
+      "ziekte van Ménière"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antibacterieel",
+      "antioxidant",
+      "schimmelwerend",
+      "anti-aging"
+    ]
+  },
+  {
+    "latijnse_naam": "Auricularia auricula-judae",
+    "familie": "Auriculariaceae",
+    "nederlandse_naam": "Judasoor",
+    "japanse_naam": "Kikurage",
+    "chinese_naam_tcm": "Heimu'er",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysaccharide mannose",
+      "Xylose",
+      "D-glucuronzuur"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "geslachtsdelen",
+      "huid",
+      "pancreas",
+      "lever",
+      "milt",
+      "zintuigen",
+      "hart en vaatstelsel",
+      "maag-darmstelsel",
+      "vrouwelijke geslachtsklieren",
+      "hersenen en zenuwstelsel",
+      "haar en haarimplant",
+      "hormonen"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antioxidant",
+      "cholesterolverlagend",
+      "regulering bloedsuikerspiegel",
+      "libidoverhogend (vrouw)",
+      "ontstekingsremmend",
+      "anti-inflammatoir",
+      "hormoonregulerend",
+      "bloeddrukverlagend"
+    ]
+  },
+  {
+    "latijnse_naam": "Coprinus comatus",
+    "familie": "Agaricaceae",
+    "nederlandse_naam": ["Inktzwam", "Aspergezwam"],
+    "japanse_naam": "Sasakure Hitoyotake",
+    "chinese_naam_tcm": "Ji Tui Mo",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Maltase",
+      "Tyrosinase",
+      "Comatin"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "pancreas",
+      "dikke darm",
+      "longen",
+      "lever",
+      "hart en vaatstelsel",
+      "bind- en steunweefsel",
+      "mannelijke geslachtsklieren"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antiviraal",
+      "regulering metabolisme",
+      "regulering darmflora",
+      "regulering bloedsuikerspiegel",
+      "antitumoraal",
+      "antioxidant"
+    ]
+  },
+
+
+  {
+    "latijnse_naam": "Cordyceps sinensis",
+    "familie": "Ophiocordycipitaceae",
+    "nederlandse_naam": "Cordyceps",
+    "japanse_naam": "Tochukaso",
+    "chinese_naam_tcm": "Dongchongxiacou",
+    "tibetaans": "Yartsa gunbu",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Cordysinins",
+      "Glutaminezuur",
+      "Cordyceptinezuren",
+      "Polyaminen",
+      "Nucleosiden",
+      "Aurantiamides",
+      "Melanine"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "longen",
+      "lever",
+      "nieren",
+      "hart en vaatstelsel",
+      "haar en haarimplant",
+      "endocriene klieren",
+      "geslachtsorganen"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "libidoverhogend (man en vrouw)",
+      "antiviraal en antibacterieel",
+      "antioxidant",
+      "ontgiftend",
+      "adaptogeen",
+      "anti-aging",
+      "anti-inflammatoir",
+      "hormoonregulerend",
+      "cognitief ondersteunend",
+      "conditieverhogend"
+    ]
+  },
+  {
+    "latijnse_naam": [
+      "Coriolus versicolor",
+      "Trametes versicolor",
+      "Polyporus versicolor"
+    ],
+    "familie": "Polyporaceae",
+    "nederlandse_naam": "Elfenbankje",
+    "japanse_naam": "Kawaratake",
+    "chinese_naam_tcm": "Yun Zhi",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysaccharide-K (PSK)",
+      "Polysaccharide-P (PSP)",
+      "Polysaccharide VSP",
+      "Beta-glucanen 1.3, 1.4 en 1.6",
+      "Ergocalciferol",
+      "Triterpenen",
+      "Coriolan"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "nieren",
+      "lever",
+      "milt",
+      "hart en vaatstelsel",
+      "longen en luchtwegen",
+      "geslachtsorganen",
+      "endocriene klieren",
+      "maag-darmstelsel",
+      "zintuigen"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antiviraal en antibacterieel",
+      "antioxidant",
+      "ontstekingsremmend",
+      "anti-inflammatoir",
+      "schimmelwerend",
+      "hormoonregulerend",
+      "bloeddrukverlagend"
+    ]
+  },
+  {
+    "latijnse_naam": "Flammulina velutipes",
+    "synoniemen": ["Collybia velutipes"],
+    "familie": "Physalacriaceae",
+    "nederlandse_naam": "Enokitake",
+    "japanse_naam": "Enokitake",
+    "chinese_naam_tcm": "Jin Zhen Gu",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Antioxidanten",
+      "Enoki-chitosan",
+      "Linolzuur",
+      "Valine",
+      "Lysine",
+      "Ergothioneïne"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "lever",
+      "pancreas",
+      "hersenen",
+      "hart en vaatstelsel",
+      "maag-darmstelsel"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antibacterieel",
+      "antitumoraal",
+      "regulering bloedsuikerspiegel",
+      "regulering metabolisme",
+      "histamine verlagend",
+      "cognitief ondersteunend"
+    ]
+  },
+  {
+    "latijnse_naam": "Fomitopsis officinalis",
+    "synoniemen": ["Laricifomes officinalis"],
+    "familie": "Fomitopsidaceae",
+    "nederlandse_naam": "Apotheker- of Purgeerzwam",
+    "japanse_naam": "Eburico",
+    "chinese_naam_tcm": "Agaricon",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Mannose",
+      "Xylose",
+      "Antioxidanten",
+      "Lipiden",
+      "Ergocalciferol",
+      "Triterpenen"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "gewrichten en spieren",
+      "zenuwstelsel",
+      "skelet",
+      "huid"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "bloeddrukregulerend",
+      "antioxidant",
+      "antiviraal en antibacterieel",
+      "ontstekingsremmend",
+      "histamine verlagend"
+    ]
+  },
+
+    {
+        "latijnse_naam": "Grifola frondosa",
+        "familie": "Meripilaceae",
+        "nederlandse_naam": "Maitake",
+        "japanse_naam": "Maitake",
+        "chinese_naam_tcm": ["Huishuhua", "Keisho"],
+        "belangrijkste_werkzame_stoffen": [
+            "Polysacchariden (grifolan)",
+            "Antioxidanten",
+            "Lipiden en fosfolipiden",
+            "Ergocalciferol",
+            "Monoxyloglucaan",
+            "Xyloglucaan",
+            "GFL (N-acetylgalactosamine-lectine)"
+        ],
+        "toepassing": [
+            "immuunsysteem",
+            "pancreas",
+            "lever",
+            "longen",
+            "huid",
+            "nieren",
+            "bloed",
+            "hart en vaatstelsel",
+            "maag-darmstelsel",
+            "endocriene klieren",
+            "geslachtsklieren"
+        ],
+        "werking": [
+            "immunomodulerend",
+            "antitumoraal",
+            "bloeddrukregulerend",
+            "regulering bloedsuikerspiegel",
+            "schimmelwerend",
+            "anti-inflammatoir",
+            "regulering metabolisme",
+            "cholesterolverlagend",
+            "antioxidant"
+        ]
+    },
+    {
+        "latijnse_naam": "Hericium erinaceus",
+        "familie": "Hericiaceae",
+        "nederlandse_naam": "Pruikzwam",
+        "japanse_naam": "Yamabushitake",
+        "chinese_naam_tcm": "Hou Tuo Gu",
+        "belangrijkste_werkzame_stoffen": [
+            "Polysaccharide HEM",
+            "Erinacine",
+            "Hericenone",
+            "Di- en triterpenen"
+        ],
+        "toepassing": [
+            "immuunsysteem",
+            "huid",
+            "hersenen en zenuwstelsel",
+            "maag-darmstelsel",
+            "endocriene klieren",
+            "spieren en gewrichten",
+            "zintuigen"
+        ],
+        "werking": [
+            "immunomodulerend",
+            "antitumoraal",
+            "antiviraal en antibacterieel",
+            "antioxidant",
+            "ontstekingsremmend",
+            "anti-inflammatoir",
+            "schimmelwerend",
+            "hormoonregulerend",
+            "cognitief ondersteunend"
+        ]
+    },
+    {
+        "latijnse_naam": "Inonotus obliquus",
+        "synoniemen": ["Fuscoporia obliqua"],
+        "familie": "Hymenochaetaceae",
+        "nederlandse_naam": "Berkenweerschijnzwam",
+        "algemene_naam": "Chaga",
+        "chinese_naam_tcm": "Bai Hua Rong",
+        "belangrijkste_werkzame_stoffen": [
+            "Polysacchariden",
+            "Betuline",
+            "Melanine",
+            "Flavonoïden (quercetine, apigenine, naringenine)",
+            "Polyfolen",
+            "Steroïden (lanosterol, inotodiol, ergocalciferol, fecosterol, episterol)"
+        ],
+        "toepassing": [
+            "immuunsysteem",
+            "lever",
+            "huid (psoriasis)",
+            "prostaat",
+            "hart en vaatstelsel",
+            "maag-darmstelsel",
+            "botten en gewrichten"
+        ],
+        "werking": [
+            "immunomodulerend",
+            "antitumoraal",
+            "antiviraal",
+            "antioxidant"
+        ]
+    },
+    {
+        "latijnse_naam": "Lentinula edodes",
+        "familie": "Marasmiaceae",
+        "nederlandse_naam": "Shiitake",
+        "japanse_naam": "Shiitake",
+        "chinese_naam_tcm": "Xiang gu",
+        "belangrijkste_werkzame_stoffen": [
+            "Lentinan",
+            "Mannoglucan",
+            "Lentinanmicin",
+            "Lentionine",
+            "Eritadenine",
+            "Ethylacetaat",
+            "Lentin",
+            "Lentysine",
+            "Lectine"
+        ],
+        "toepassing": [
+            "immuunsysteem",
+            "lever",
+            "longen",
+            "pancreas",
+            "bloed",
+            "zintuigen",
+            "grote hersenen",
+            "hart en vaatstelsel",
+            "maag-darmstelsel",
+            "spieren en gewrichten",
+            "skelet",
+            "gebit"
+        ],
+        "werking": [
+            "immunomodulerend",
+            "antitumoraal",
+            "antiviraal en antibacterieel",
+            "antioxidant",
+            "ontstekingsremmend",
+            "anti-inflammatoir",
+            "schimmelwerend",
+            "regulering bloedsuikerspiegel",
+            "bloeddrukverlagend"
+        ]
+    },
+
+{
+  "latijnse_naam": "Lignosus rhinocerus",
+  "familie": "Polyporaceae",
+  "nederlandse_naam": "Tijgermelk",
+  "internationale_naam": "Tiger Milk",
+  "maleisische_naam": "Cendawan Susu Rimau",
+  "belangrijkste_werkzame_stoffen": [
+    "Polysacchariden",
+    "Antioxidanten",
+    "Fenol",
+    "Triterpenen",
+    "Sclerotium"
+  ],
+  "toepassing": [
+    "immuunsysteem",
+    "longen",
+    "lever",
+    "maag",
+    "nieren",
+    "wondbehandeling"
+  ],
+  "werking": [
+    "immunomodulerend",
+    "antitumoraal",
+    "antiproliferatief",
+    "antioxidant",
+    "antiviraal en antibacterieel",
+    "ontstekingsremmend"
+  ],
+
+  {
+    "latijnse_naam": "Phellinus linteus",
+    "familie": "Hymenochaetaceae",
+    "nederlandse_naam": "Phellinus linteus",
+    "japanse_naam": ["Mesima", "Meshimakobu"],
+    "chinese_naam_tcm": "Song-gen",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Protocatechuïnezuur",
+      "Cafeïnezuur",
+      "Hispidin",
+      "Hypholomine B",
+      "Interfungis A",
+      "Inoscavin A",
+      "Hispolon"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "lever",
+      "longen",
+      "pancreas",
+      "huid",
+      "prostaat",
+      "hart en vaatstelsel",
+      "botten en gewrichten"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antioxidant",
+      "bloedsuikerspiegel regulerend",
+      "antiviraal en antibacterieel",
+      "histamineverlagend"
+    ],
+
+  {
+    "latijnse_naam": "Pleurotus ostreatus",
+    "familie": "Pleurotaceae",
+    "nederlandse_naam": "Oesterzwam",
+    "japanse_naam": "Hiratake",
+    "chinese_naam_tcm": "Pinggu",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Ergothioneïne",
+      "Benzaldehyde",
+      "Lovastatine"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "lever",
+      "longen",
+      "pancreas",
+      "hart en vaatstelsel",
+      "mannelijke geslachtsklieren",
+      "spieren en gewrichten"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antiviraal en antibacterieel",
+      "histamine verlagend",
+      "ontstekingsremmend",
+      "anti-inflammatoir",
+      "anti-atherogeen"
+    ],
+
+
+  {
+    "latijnse_naam": "Polyporus umbellatus",
+    "familie": "Polyporaceae",
+    "nederlandse_naam": "Eikhaas",
+    "japanse_naam": "Chorei",
+    "chinese_naam_tcm": "Zhuling",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Antioxidanten",
+      "Lipiden en fosfolipiden",
+      "Ergosterol",
+      "Ecdysteroïden",
+      "Mono-xyloglucan",
+      "Xyloglucan"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "zintuigen",
+      "nieren",
+      "blaas",
+      "lever",
+      "longen",
+      "milt",
+      "lymfen",
+      "hart en vaatstelsel",
+      "maag-darmstelsel",
+      "haar en haarimplant"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antiviraal en antibacterieel",
+      "schimmelwerend",
+      "anti-inflammatoir",
+      "ontstekingsremmend"
+    ],
+
+  {
+    "latijnse_naam": "Poria cocos",
+    "synoniemen": ["Wolfiporia extensa"],
+    "familie": "Polyporaceae",
+    "nederlandse_naam": "Poria cocos",
+    "japanse_naam": "Bukuryo Matsuhodo",
+    "chinese_naam_tcm": "Fu ling",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysacchariden",
+      "Ergocalciferol",
+      "Caprylzuur",
+      "Laurinezuur",
+      "Fungisterol",
+      "Choline",
+      "Fosfolipiden"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "pancreas",
+      "lever",
+      "longen",
+      "huid",
+      "prostaat",
+      "nieren",
+      "hart en vaatstelsel",
+      "maag-darmstelsel"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antioxidant",
+      "regulering bloedsuikerspiegel",
+      "antiviraal",
+      "regulering metabolisme",
+      "ontstekingsremmend",
+      "histamineverlagend",
+      "hiv- en aids-gerelateerde aandoeningen"
+    ],
+
+  {
+    "latijnse_naam": "Schizophyllum commune",
+    "familie": "Schizophyllaceae",
+    "nederlandse_naam": "Waaiertje",
+    "japanse_naam": "Suehirotake",
+    "chinese_naam_tcm": "Ba dan Chai",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysaccharide schizophyllan (SPG)",
+      "Polysaccharide sonifilan",
+      "Polysaccharide sizofiran",
+      "Arabinoxylaan"
+    ],
+    "toepassing": [
+      "immuunsysteem",
+      "huidaandoeningen",
+      "vrouwelijke geslachtsorganen",
+      "menopauze",
+      "reuma",
+      "lymfesysteem"
+    ],
+    "werking": [
+      "immunomodulerend",
+      "antitumoraal",
+      "antibacterieel en antiviraal",
+      "antioxidant",
+      "endocriene klieren",
+      "schimmelwerend",
+      "ontstekingsremmend",
+      "anti-inflammatoir"
+    ],
+  {
+    "latijnse_naam": "Tremella fuciformis",
+    "familie": "Tremellaceae",
+    "nederlandse_naam": "Zilveroortje",
+    "verenigde_staten_naam": "Snow fungus",
+    "japanse_naam": ["Shirokikurage", "Hakumokuji"],
+    "chinese_naam_tcm": "Yin Er",
+    "belangrijkste_werkzame_stoffen": [
+      "Polysaccharide WTF-B",
+      "Erinacine",
+      "Hericenone",
+      "Triterpenen"
+    ],
+    "toepassing": [
+      "lever",
+      "huid",
+      "pancreas",
+      "nieren",
+      "zenuwen",
+      "longen en luchtwegen",
+      "hersenen (cognitief)",
+      "hart en vaatstelsel",
+      "maag-darmstelsel",
+      "skelet"
+    ],
+    "werking": [
+      "anti-aging",
+      "immunomodulerend",
+      "antitumoraal",
+      "hydraterend",
+      "regulering bloedsuikerspiegel"
+    ]
+  }
+]
+
